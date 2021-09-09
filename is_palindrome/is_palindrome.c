21eb225430eac2f7ad9ffd7fd4ed3e1b3a2a2a85
  Чиковани Виктор <cikovani_vy@MacBook-Pro-Administrator-2.local>
2021-09-10 00:26:52 +0300

    test

--- is_palindrome/is_palindrome.c
+++ is_palindrome/is_palindrome.c
@@ -1,27 +1,42 @@
 #include <stdio.h>
+#include <string.h>
 
 char * input_text() {
     const int SIZE = 50;    
-    char word[SIZE];
+    char raw_word[SIZE];
+
+    printf("Enter the word: \n");
+    scanf("%s", raw_word);
+
+    int len = 0;
+    while(raw_word[len] != '\0')
+        len++;
+    len++;
+    char word[len];
+    memcpy(word, raw_word, len);
     char *p = word;
-
-    puts("Enter the word: ");
-    gets(word);
-
     return p;
 }
 
 int main(void) {
 
     char *word = input_text();
+    printf("Got text\n");
+    int len = 0;
+    while(word[len] != '\0')
+        len++;
+    printf("%d", len);
 
-    int word_len = sizeof(word) / sizeof(word[0]);    
-    for(int i = 0; i < word_len; i++) {
-        if(word[i] == '\0') {
-            break;
+    int left = 0;
+    int middle = len / 2;
+    int right = len - 1;
+    while(word[left++] == word[right--]) {
+        if(left = middle - 1 ) {
+            ;
         }
-        printf("%c", word[i]);
     }
+    
+
     return 0;
 }
    
