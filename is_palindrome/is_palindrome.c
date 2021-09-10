dad7215bafe3093417bcb319fcbd72b063f67b97
  Чиковани Виктор <cikovani_vy@MacBook-Pro-Administrator-2.local>
2021-09-10 13:47:33 +0300

    test

--- is_palindrome/is_palindrome.c
+++ is_palindrome/is_palindrome.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
-char * input_text() {
+char *input_text()
+{
     const int SIZE = 50;    
     char raw_word[SIZE];
 
@@ -18,25 +19,33 @@ char * input_text() {
     return p;
 }
 
-int main(void) {
+int str_len(char *str)
+{
+    int len = 0;
+    while(str[len] != '\0')
+        len++;
+    return len;
+}
+
+int is_palindrome(char *word)
+{
+    int len = str_len(word);
+
+    int left = 0, right = len - 1;
+    while(word[left++] == word[right--])
+    {
+        if(left == len / 2)
+            return 1;
+    }
+    return 0;
+}
+
+int main(void)
+{
 
     char *word = input_text();
-    printf("Got text\n");
-    int len = 0;
-    while(word[len] != '\0')
-        len++;
-    printf("%d", len);
-
-    int left = 0;
-    int middle = len / 2;
-    int right = len - 1;
-    while(word[left++] == word[right--]) {
-        if(left = middle - 1 ) {
-            ;
-        }
-    }
-    
-
+    int answer = is_palindrome(word);
+    printf("Word %s is palindrome: %d\n", word, answer); 
     return 0;
 }
    
